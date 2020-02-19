2042fea69fe59d43ea1a42ed9f2fb69c837b1add
anthony2698 <carrilloanthony2698@gmail.com>
2020-02-19 18:08:44 -0500

    Finished SearchBar.

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,16 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  console.log(props.comments);
+  const [comments, setComments] = useState(props.comments);
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {/* map through the comments data and return the Comment component */
+      props.comments.map(el => {
+        return (<Comment comment={el} />)
+      })
+      }
       <CommentInput />
     </div>
   );
